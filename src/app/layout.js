99512c6f5266e40efdf07c3748dd725eab69cb3d
Insy Hernsvan <insy.hernsvan@envisionpharma.com>
2025-04-03 16:18:09 -0400

    Install next.js and tailwind css and create the first template

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from '@/components/Header';
-import { ClerkProvider, ClerkLoaded, ClerkLoading } from '@clerk/nextjs';
-import Loader from '@/components/Loader';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
